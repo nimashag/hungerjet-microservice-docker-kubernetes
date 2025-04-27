cb2bc296aaf92cbb954ec1361977c93d81b0ab7c
nayanahari <nayanaharikusalanajani@gmail.com>
2025-04-27 20:54:56 +0530

    update fetchRestaurant end poin

--- orders-service/src/api/restaurant.api.ts
+++ orders-service/src/api/restaurant.api.ts
@@ -1,13 +1,15 @@
 import axios from 'axios';
+import dotenv from 'dotenv';
+dotenv.config();
 
 const RESTAURANT_SERVICE_URL = process.env.RESTAURANT_SERVICE_URL;
 
 export const fetchMenuItems = async (restaurantId: string) => {
-  const res = await axios.get(`http://localhost:3001/api/restaurants/${restaurantId}/menu-items`);
+  const res = await axios.get(`${RESTAURANT_SERVICE_URL}/api/restaurants/${restaurantId}/menu-items`);
   return res.data;
 };
 
 export const fetchRestaurant = async (restaurantId: string) => {
-  const res = await axios.get(`http://localhost:3001/api/restaurants/${restaurantId}`);
+  const res = await axios.get(`${RESTAURANT_SERVICE_URL}/api/restaurants/${restaurantId}`);
   return res.data;
 };
